7912e95644d73240a30e46c6f574f0532931b28c
sfomuseumbot <sfomuseumbot@localhost>
2024-12-09 17:49:06 -0800

    start accounting for update method defs

--- response/method.go
+++ response/method.go
@@ -1,10 +1,56 @@
 package response
 
+import (
+	"fmt"
+	"log/slog"
+	"strconv"
+)
+
 type Parameter struct {
 	Name        string `json:"name"`
+	Type        string `json:"type"`
 	Description string `json:"description"`
 	Required    int    `json:"required"` // sudo make me a boolean
 	Example     any    `json:"example"`  // sudo make me a string
+}
+
+func (p *Parameter) String() (string, error) {
+
+	switch p.Type {
+	case "int64":
+
+		i, ok := p.Example.(int64)
+
+		if !ok {
+			return "", fmt.Errorf("Not a valid int64 (%T)", p.Example)
+		}
+
+		return strconv.FormatInt(i, 10), nil
+
+	case "float64":
+
+		f, ok := p.Example.(float64)
+
+		if !ok {
+			return "", fmt.Errorf("Not a float64 (%T)", p.Example)
+		}
+
+		return strconv.FormatFloat(f, 'g', -1, 64), nil
+
+	case "string":
+
+		s, ok := p.Example.(string)
+
+		if !ok {
+			return "", fmt.Errorf("Not a string (%T)", p.Example)
+		}
+
+		return s, nil
+
+	default:
+		slog.Warn("Unhandled API parameter type", "type", p.Type)
+		return fmt.Sprintf("%v", p.Example), nil
+	}
 }
 
 type Error struct {
