d186071f3d92c7275abc3da50357b244b4e344ed
sfomuseumbot <sfomuseumbot@localhost>
2024-12-10 10:13:48 -0800

    type wrangling

--- response/method.go
+++ response/method.go
@@ -10,8 +10,8 @@ type Parameter struct {
 	Name        string `json:"name"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
-	Required    int    `json:"required"` // sudo make me a boolean
-	Example     any    `json:"example"`  // sudo make me a string
+	Required    bool    `json:"required"`
+	Example     any    `json:"example"`
 }
 
 func (p *Parameter) String() (string, error) {
@@ -55,17 +55,17 @@ func (p *Parameter) String() (string, error) {
 
 type Error struct {
 	Message    string `json:"message"`
-	Documented int    `json:"documented"`
+	Documented bool    `json:"documented"`
 }
 
 type Method struct {
 	Name                   string      `json:"name"`
 	RequestMethod          string      `json:"request_method"`
 	Description            string      `json:"description"`
-	RequiresAuthentication int         `json:"requires_auth"` // sudo make me a boolean
+	RequiresAuthentication bool         `json:"requires_auth"` // sudo make me a boolean
 	Parameters             []Parameter `json:"parameters"`
 	// Errors                 map[string]Error `json:"errors"`
 	Notes     []string `json:"notes"`
-	Extras    int      `json:"extras"`    // sudo make me a boolean
-	Paginated int      `json:"paginated"` // sudo make me a boolean
+	Extras    bool      `json:"extras"`   
+	Paginated bool      `json:"paginated"`
 }
