2595ad24ac115030abf40df5d1e66117160969f1
sfomuseumbot <sfomuseumbot@localhost>
2024-12-18 13:57:41 -0800

    update godoc for /response

--- response/images.go
+++ response/images.go
@@ -4,18 +4,27 @@ import (
 	"strings"
 )
 
+// ImageCaptionResponse defines the response object returned by the `sfomuseum.collection.images.getCaption` API method.
 type ImageCaptionResponse struct {
+	// Caption is an `ImageCaption` instance.
 	Caption *ImageCaption
 }
 
+// ImageCaption defines the component parts of a caption for an object image in the SFO Museum Aviation Collection.
 type ImageCaption struct {
-	Title           string `json:"title"`
-	Date            string `json:"date"`
-	CreditLine      string `json:"creditline"`
+	// Title is the title of the object.
+	Title string `json:"title"`
+	// Date is the date attributed to the object.
+	Date string `json:"date"`
+	// Creditline is the credit line for the object.
+	CreditLine string `json:"creditline"`
+	// AccessionNumber if the object's SFO Museum accession number.
 	AccessionNumber string `json:"accession_number"`
-	URL             string `json:"url"`
+	// URL is the collection.sfomuseum.org URL for the object.
+	URL string `json:"url"`
 }
 
+// String() returns the image caption for an object image in the SFO Museum Aviation Collection as a line-separated string.
 func (r *ImageCaption) String() string {
 
 	lines := []string{
--- response/shoebox.go
+++ response/shoebox.go
@@ -1,14 +1,23 @@
 package response
 
+// ShoeboxListItemsResponse defines the reponse object returned by the `sfomuseum.you.shoebox.listItems` API method.
 type ShoeboxListItemsResponse struct {
+	// Zero or `ShoeboxListItem` instances.
 	Items []*ShoeboxListItem `json:"items"`
 }
 
+// ShoeboxListItem defines an individual (shoebox) item returned by the `sfomuseum.you.shoebox.listItems` API method.
 type ShoeboxListItem struct {
-	Id           int64 `json:"id"`
-	ItemId       int64 `json:"item_id"`
-	Status       uint8 `json:"status"`
-	Created      int64 `json:"created"`
+	// The unique identifier for the shoebox item.
+	Id int64 `json:"id"`
+	// The unique identifier of the item added to a person's shoebox.
+	ItemId int64 `json:"item_id"`
+	// The numeric status code of the shoebox item.
+	Status uint8 `json:"status"`
+	// The Unix timestamp when the item was added the shoebox.
+	Created int64 `json:"created"`
+	// The Unix timestamp when the shoebox item was last modified.
 	LastModified int64 `json:"lastmodified"`
-	TypeId       uint8 `json:"type_id"`
+	// TypeId is the numeric identifier of the type of item added to the shoebox.
+	TypeId uint8 `json:"type_id"`
 }
